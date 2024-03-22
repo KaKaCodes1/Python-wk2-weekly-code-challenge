ab8cda3c9af5ebc963efdf86483b228df8ddfc1c
HAMISI <hamisig123@gmail.com>
2024-03-22 13:20:08 +0300

    Made changes to list.py

--- list.py
+++ list.py
@@ -5,17 +5,21 @@ my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
+print(f"The list with appended values:\n {my_list}")
 #Insert the value 15 at the second position in the list.
 my_list.insert(1,15)
+print(f"The list with the value 15 inserted at the second position:\n {my_list}")
 #Extend my_list with another list: [50, 60, 70].
 new_list=[50,60,70]
 my_list.extend(new_list)
+print(f"The list extended with another list:\n {my_list}")
 #Remove the last element from my_list.
 my_list.pop(-1)
+print(f"The list with the last element removed:\n {my_list}")
 #Sort my_list in ascending order.
 my_list.sort()
+print(f"The list sorted in ascending order:\n {my_list}")
 #Find and print the index of the value 30 in my_list.
-print(my_list[3])
+print("the index of the value 30 in my_list:\n",my_list.index(30))
 
 
-print(my_list)
